35daa1ff8baef397d5246504cd5e17bd9999dc62
vishilp <vishilthe3rd@gmail.com>
2024-12-26 21:23:01 -0800

    styling for intro component

--- my-website/src/components/Intro/intro.jsx
+++ my-website/src/components/Intro/intro.jsx
@@ -5,8 +5,9 @@ return(
     <div className='container'> 
         <div className='textcontainer'>
             <h1 className="hi">hi, i'm vishil👋</h1>
-            <p className='info'>I'm currently pursuing a Master's degree at UCLA studying Data 
-            Science in Biomedicine. My undergraduate degree was in Computational Biology. </p>
+            <p className='info1'>22 year old software developer based in Los Angeles</p>
+            <p className='info2'>I'm currently pursuing a Master's degree at UCLA studying Data 
+            Science in Biomedicine. My BS was also obtained in UCLA, with the Computational and Systems Biology major. </p>
         </div>
         <img className="picture" src="https://i.imgur.com/VOU2EEl.png"/>
     </div>
