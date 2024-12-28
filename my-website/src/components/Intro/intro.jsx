c3b43b96061947276b62946eebda7cfa71e66f10
vishilp <vishilthe3rd@gmail.com>
2024-12-28 15:48:17 -0800

    added link to resume and modular css

--- my-website/src/components/Intro/intro.jsx
+++ my-website/src/components/Intro/intro.jsx
@@ -1,16 +1,16 @@
-import './styles.css'
+import styles from './styles.module.css'
 
 function Intro(){
 return(
-    <div className='container'> 
-        <div className='textcontainer'>
-            <h1 className="hi">hi, i'm vishil👋</h1>
-            <p className='info1'>22 year old data science graduate student at UCLA</p>
-            <p className='info2'>I tinker with fun personal projects, lift heavy circles,
+    <div className={styles.container}> 
+        <div className={styles.textcontainer}>
+            <h1 className={styles.hi}>hi, i'm vishil👋</h1>
+            <p className={styles.info1}>22 year old data science graduate student at UCLA</p>
+            <p className={styles.info2}>I tinker with fun personal projects, lift heavy circles,
                 and ocassionally indulge in donuts and cookies.
             </p>
         </div>
-        <img className="picture" src="https://i.imgur.com/WMEpDkQ.png"/>
+        <img className={styles.picture} src="https://i.imgur.com/WMEpDkQ.png"/>
     </div>
 )
 }
--- my-website/src/components/SocialLinks/sociallinks.jsx
+++ my-website/src/components/SocialLinks/sociallinks.jsx
@@ -1,7 +1,12 @@
+import PDF from '../../assets/Resume.pdf'
+import styles from './styles.module.css'
+
 function SocialLinks(){
     return(
-        <div className="container">
-            <h1>hi</h1>
+        <div className={styles.container}>
+            <h1>Resume</h1>
+            <a href={PDF} target="_blank">Resume</a>
+            <h2>hello</h2>
         </div>
     )
 }
