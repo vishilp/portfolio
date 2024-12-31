a5c8a077f55cfb49dfb5c8fb8444d0fe6043cbd3
vishilp <vishilthe3rd@gmail.com>
2024-12-30 22:02:04 -0800

    working on work tablist component

--- my-website/src/components/Work_Tablist/work_tablist.jsx
+++ my-website/src/components/Work_Tablist/work_tablist.jsx
@@ -1,7 +1,24 @@
+import styles from './styles.module.css'
+import uclaicon from '../../assets/ucla.png'
+
 function Work_tablist(){
     return(
-        <div style={{backgroundColor: 'black', height: '500px', width: '500px'}}>
-            
+        <div className={styles.container}>
+            <div className={styles.workContainer}>
+                <img className={styles.icon} src ={uclaicon}/>
+                <div className={styles.textContainer}>
+                    <p className={styles.date}> Sept 2023 - Sept 2024</p>
+                    <p className={styles.location}>University of California, Los Angeles</p>
+                    <p className={styles.role}>Software Developer</p>
+                    <ul className={styles.description}>
+                        <li>Developed utilizing Python and the Streamlit framework. Focused on organizing the code to allow future developers to update the tool with minimal effort.</li>
+                        <li>Showcases an extensive directory of molecular signatures from leading databases like MSigDB and Enrichr. </li>
+                    </ul>
+                </div>
+            </div>
+            <div className={styles.workContainer}>
+
+            </div>
         </div>
     )
 }
