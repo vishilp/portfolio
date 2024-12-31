1ee651647a0e32c13c45c8df65396dc9995fe6a5
vishilp <vishilthe3rd@gmail.com>
2024-12-31 10:45:28 -0800

    added routing and created a navbar component

--- my-website/src/App.jsx
+++ my-website/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import HomePage from './pages/HomePage/HomePage'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      <HomePage/>
-    </>
+    <main className='main-content'>
+      <Routes>
+        <Route path="/" element = {<HomePage/>}></Route>
+        <Route path="/projects" element = {<HomePage/>}></Route>
+        <Route path="/blog" element = {<HomePage/>}></Route>
+        <Route path="/contact" element = {<HomePage/>}></Route>
+      </Routes>
+    </main>
   )
 }
 
---
+++ my-website/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import styles from "./styles.modules.css"
+
+function NavBar(){
+    return(
+        <nav className={styles.navbar}>
+            <div className={styles.navbarlinks}>
+                <Link to="/" className={styles.link} >home</Link>
+                <Link to="/projects" className={styles.link} >projects</Link>
+                <Link to="/blog" className={styles.link} >blog</Link>
+                <Link to="/contact" className={styles.link} >contact</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
