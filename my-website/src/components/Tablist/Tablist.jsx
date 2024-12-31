c9b694bd4d271066a27cef04d5910734ecd204c0
vishilp <vishilthe3rd@gmail.com>
2024-12-30 19:09:48 -0800

    added a state system to render component based on tab clicked

--- my-website/src/components/Tablist/Tablist.jsx
+++ my-website/src/components/Tablist/Tablist.jsx
@@ -1,11 +1,24 @@
 import styles from './styles.module.css'
+import Work_tablist from '../Work_Tablist/work_tablist'
+import Education_tablist from '../Education_Tablist/education_tablist'
+import { useState } from 'react'
 
 function Tablist(){
+    const [activeTab, setActiveTab] = useState("Work")
+    const setWork = () => {
+        setActiveTab("Work")
+    }
+    const setEducation = () => {
+        setActiveTab("Education")
+    }
     return(
+        <>
         <div className={styles.container}>
-            <button className={styles.button}>Work</button>
-            <button className={styles.button}>Education</button>
+            <button className={styles.button} onClick={setWork}>Work</button>
+            <button className={styles.button} onClick={setEducation}>Education</button>
         </div>
+        {activeTab === "Work" ? <Work_tablist/> : <Education_tablist/>}
+        </>
     )
 }
 
