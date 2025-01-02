30fddb692e627ca97883e80a628c20b55781f353
vishilp <vishilthe3rd@gmail.com>
2025-01-02 12:36:42 -0800

    useState and sun/moon rendering based on state

--- my-website/src/components/NavBar/NavBar.jsx
+++ my-website/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css"
+import { RxMoon } from "react-icons/rx";
+import { LuSunMedium } from "react-icons/lu";
+import { useState } from "react";
 
 function NavBar(){
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () =>{
+        const newTheme = (theme === 'light' ? 'dark' : 'light');
+        setTheme(newTheme)
+    }
     return(
         <nav className={styles.navbar}>
             <div className={styles.navbarlinks}>
@@ -10,6 +19,10 @@ function NavBar(){
                 <Link to="/blog" className={styles.link} >blog</Link>
                 <Link to="/contact" className={styles.link} >contact</Link>
             </div>
+            <button onClick={toggleTheme} className={styles.switch}>
+                {theme === 'light' ? <RxMoon size="20px" style={{color: 'rgb(157, 113, 238)'}}/> 
+                : <LuSunMedium size="20px" style={{color: 'rgb(214, 204, 56)'}}/>}
+            </button>
         </nav>
     )
 }
