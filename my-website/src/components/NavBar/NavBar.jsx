dff98e8cc4cfc95e9befdcb0c1591b3b88b76a2e
vishilp <vishilthe3rd@gmail.com>
2025-01-02 13:33:06 -0800

    working on intro color scheme

--- my-website/src/components/NavBar/NavBar.jsx
+++ my-website/src/components/NavBar/NavBar.jsx
@@ -2,14 +2,23 @@ import { Link } from "react-router-dom";
 import styles from "./styles.module.css"
 import { RxMoon } from "react-icons/rx";
 import { LuSunMedium } from "react-icons/lu";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function NavBar(){
     const [theme, setTheme] = useState('light');
 
+    useEffect(() => {
+        // Check the current theme from localStorage or use system preference
+        const savedTheme = localStorage.getItem('theme') || 'light';
+        setTheme(savedTheme);
+        document.documentElement.setAttribute('data-theme', savedTheme);
+      }, []);
+
     const toggleTheme = () =>{
         const newTheme = (theme === 'light' ? 'dark' : 'light');
-        setTheme(newTheme)
+        setTheme(newTheme);
+        document.documentElement.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
     }
     return(
         <nav className={styles.navbar}>
