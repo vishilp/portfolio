bc99b1e2c9894939be98920acd5c5006acf83377
vishilp <vishilthe3rd@gmail.com>
2025-01-01 23:13:20 -0800

    done with featured projects section

--- my-website/src/components/Featured_Projects/featured_projects.jsx
+++ my-website/src/components/Featured_Projects/featured_projects.jsx
@@ -19,20 +19,33 @@ function Featured_Projects(){
                     <img src= {PacMan} className={styles.picture}/>
                     <div className={styles.cardholder}>
                         <p className={styles.title}>PacMan Clone</p>
-                        <p className={styles.description}>C++ clone that covers game development, 
+                        <p className={styles.description}>Project that covers game development, graph search algorithms,
                             dynamic memory allocation, polymorphism, object oriented design and artificial intelligence.</p>
                         <div className={styles.tools}>
-                            
+                            <p className={styles.tool}>C</p>
+                            <p className={styles.tool}>C++</p>
+                            <p className={styles.tool}>SDL2</p>
                         </div>
-                        <button className={styles.sourceCode}>Source</button>
+                        <a href="https://github.com/vishilp/pac-man" target="_blank">
+                            <button className={styles.sourceCode}>Source</button>
+                        </a>
                     </div>
                 </div>
                 <div className={styles.projectsquare}>
-                    <img src={Realtor} className={styles.picture}/>
-                    <p className={styles.title}>Realtor Information Scraper</p>
-                    <p className={styles.description}></p>
-                    <div className={styles.tools}></div>
-                    <button className={styles.sourceCode}></button>
+                    <img src= {Realtor} className={styles.picture}/>
+                    <div className={styles.cardholder}>
+                        <p className={styles.title}>Realtor Information Scraper</p>
+                        <p className={styles.description}>Automated finding property information for a small business leading to increase in customer
+                        acquisition, sales, and productivity. Investigated techniques for browser automation and scraping.</p>
+                        <div className={styles.tools}>
+                            <p className={styles.tool}>Python</p>
+                            <p className={styles.tool}>Selenium</p>
+                            <p className={styles.tool}>Google API</p>
+                        </div>
+                        <a href="https://github.com/vishilp/SQFTFinder" target="_blank">
+                            <button className={styles.sourceCode}>Source</button>
+                        </a>
+                    </div>
                 </div>
 
             </div>
