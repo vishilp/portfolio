0dfb5463bfd62fe534f82b93348838f4a9b96bcc
vishilp <vishilthe3rd@gmail.com>
2025-01-01 21:52:02 -0800

    working on featured projects section

--- my-website/src/components/Featured_Projects/featured_projects.jsx
+++ my-website/src/components/Featured_Projects/featured_projects.jsx
@@ -1,9 +1,41 @@
 import styles from './styles.module.css'
+import { Link } from 'react-router-dom'
+import { FaArrowRight } from "react-icons/fa";
+import PacMan from '../../assets/pac-man.jpg'
+import Realtor from '../../assets/realtor.jpeg'
 
 function Featured_Projects(){
     return(
         <div className={styles.container}>
+            <div className={styles.header}>
+                <h1 className={styles.headertext}>featured projects</h1>
+                <div className={styles.linkcontainer}>
+                    <Link to="/projects" className={styles.link} >view more</Link>
+                    <FaArrowRight className={styles.arrow} size= "16px"/>
+                </div>
+            </div>
+            <div className={styles.projects}>
+                <div className={styles.projectsquare}>
+                    <img src= {PacMan} className={styles.picture}/>
+                    <div className={styles.cardholder}>
+                        <p className={styles.title}>PacMan Clone</p>
+                        <p className={styles.description}>C++ clone that covers game development, 
+                            dynamic memory allocation, polymorphism, object oriented design and artificial intelligence.</p>
+                        <div className={styles.tools}>
+                            
+                        </div>
+                        <button className={styles.sourceCode}>Source</button>
+                    </div>
+                </div>
+                <div className={styles.projectsquare}>
+                    <img src={Realtor} className={styles.picture}/>
+                    <p className={styles.title}>Realtor Information Scraper</p>
+                    <p className={styles.description}></p>
+                    <div className={styles.tools}></div>
+                    <button className={styles.sourceCode}></button>
+                </div>
 
+            </div>
         </div>
     )
 }
