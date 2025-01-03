88bfa9f2506135fc605dc5187918f6f595c80f7c
vishilp <vishilthe3rd@gmail.com>
2025-01-03 00:21:55 -0800

    web3 forms contact form done, just styling left

--- my-website/src/components/Contact_Form/contact_form.jsx
+++ my-website/src/components/Contact_Form/contact_form.jsx
@@ -1,10 +1,36 @@
 import styles from './styles.module.css'
+import { useState } from 'react';
 
 function Contact_Form(){
+    const [result, setResult] = useState("Send Message");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "33c6ffdf-ab1d-4ff6-8c8d-1c55700ab407");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+        } else {
+        console.log("Error", data);
+        setResult(data.message);
+        }
+    };
+
     return(
+        
         <div className={styles.container}>
-            <form className={styles.form} action="https://api.web3forms.com/submit" method="POST">
-            <input type="hidden" name="access_key" value="33c6ffdf-ab1d-4ff6-8c8d-1c55700ab407"></input>
+            <form className={styles.form} onSubmit={onSubmit}>
                 <div className={styles.top}>
                     <input className={styles.row1} type="text" required placeholder='Name' name='Name'></input>
                     <input className={styles.row1} type="text" required placeholder='Email' name='Email'></input>
@@ -13,10 +39,11 @@ function Contact_Form(){
                     <textarea className={styles.row2} type="text" required placeholder='Send anything my way, be it work opportunities, criticism, or just a plain old hello...' name='Msg'></textarea>
                 </div>
                 <div className={styles.button}>
-                    <input className={styles.submit} type="submit"></input>
+                    <input className={styles.submit} type="submit" value={result}></input>
                 </div>
             </form>
         </div>
+        
     )
 }
 
