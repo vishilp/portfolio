8e71e4e83892f44d713112bd6c75df782844ee93
vishilp <vishilthe3rd@gmail.com>
2025-01-02 23:46:39 -0800

    working on form submit button

--- my-website/src/components/Contact_Form/contact_form.jsx
+++ my-website/src/components/Contact_Form/contact_form.jsx
@@ -3,14 +3,17 @@ import styles from './styles.module.css'
 function Contact_Form(){
     return(
         <div className={styles.container}>
-            <form className={styles.form}>
-                <div>
-                    <input className={styles.row1} type="text" required placeholder='Name' name='fname'></input>
-                    <input className={styles.row1} type="text" required placeholder='Email' name='femail'></input>
+            <form className={styles.form} action="https://api.web3forms.com/submit" method="POST">
+            <input type="hidden" name="access_key" value="33c6ffdf-ab1d-4ff6-8c8d-1c55700ab407"></input>
+                <div className={styles.top}>
+                    <input className={styles.row1} type="text" required placeholder='Name' name='Name'></input>
+                    <input className={styles.row1} type="text" required placeholder='Email' name='Email'></input>
                 </div>
-                <div>
-                    <input className={styles.row2} type="text" required placeholder='Write me anything and 
-                    I will get back to you when I can' name='fmsg'></input>
+                <div className={styles.bottom}>
+                    <textarea className={styles.row2} type="text" required placeholder='Send anything my way, be it work opportunities, criticism, or just a plain old hello...' name='Msg'></textarea>
+                </div>
+                <div className={styles.button}>
+                    <input className={styles.submit} type="submit"></input>
                 </div>
             </form>
         </div>
