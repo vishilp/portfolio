b229eabbe6567f086387dd619333d8d49ae92226
vishilp <vishilthe3rd@gmail.com>
2025-01-04 11:25:43 -0800

    added scroll to top function for react routers

--- my-website/src/main.jsx
+++ my-website/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import ScrollToTop from './components/ScrolltoTop/scrolltotop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <ScrollToTop/>
       <App />
     </BrowserRouter>
   </StrictMode>,
